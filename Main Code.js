f8cb2990fd0044781ac394d83b1d77d4107e2ed0
Geonwoo1109 <74478101+Geonwoo1109@users.noreply.github.com>
2021-06-23 23:49:00 +0900

    Update Main Code.js

--- Main Code.js
+++ Main Code.js
@@ -1,3 +1,10 @@
 const mainPath = "/sdcard/ChatRank/";
 const Fs = FileStream;
 
+var json = JSON.parse(Fs.read(mainPath+room)).sender + 1;
+/*
+1 추가한 후 바로 정렬 돌리고 불러올 때 편하게 불러오기
+*/
+
+if (msg == ".채팅순위") {
+  
