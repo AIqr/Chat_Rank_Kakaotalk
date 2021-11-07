f9e3be5549536b21065a86c683ffec3cb2935d6d
Geonwoo1109 <74478101+Geonwoo1109@users.noreply.github.com>
2021-11-07 13:55:09 +0900

    Update Main Code.js

--- Main Code.js
+++ Main Code.js
@@ -1,32 +1,67 @@
-const mainPath = "/sdcard/ChatRank/";
+const scriptName = "챗순위";
+
 const Fs = FileStream;
-const allsee = "\u200d".repeat(500);
-const nn = "\n\n";
+const Path = "/sdcard/ChatRank/";
 
-var json = {};
-json[room] = room;
+var file = {};
+var arr = [];
 
-var time = 
-
-
-
-var json = JSON.parse(Fs.read(mainPath+room)).sender + 1;
-/*
-1 추가한 후 바로 정렬 돌리고 불러올 때 편하게 불러오기
-2. 메시지 오면 일단 저장하고 불러올때 정렬 -> 이게 더 메모리 적게먹을듯?
-*/
-
-if (msg == ".채팅순위") {
-  //
-  replier.reply(
-    "채팅 순위입니다" + allsee
-  + "".sort() //자동정렬?
-  );
-}
+function response(room, msg, sender, isGroupChat, replier, imageDB, packageName) {
+  try {
+   //if (room != "카톡봇 테스트방") return;
+    
+    if (!Fs.read(Path + room)) reset(room);
+    if (msg == ".ㅅㅇㄹㅅ") reset(room)
+    
+    file = JSON.parse(Fs.read(Path + room));
+    
+    if (!file["today"].find(e => e.name == sender)) {
+      file["today"].push(
+      {
+        "name": sender,
+        "times": 1
+      }
+      );
+    } else {
+      file["today"].find(e => e.name == sender).times += 1;
+    }
+    
+    
+    Fs.write(Path + room, JSON.stringify(file));
+    
+    
+    if (msg == ".ㅌ") {
+      replier.reply(JSON.stringify(file, null, 4))
+      replier.reply(JSON.stringify(file["total"].find(e => e.name == sender).score))
+    
+    }
   
-if (msg == ".채팅순위리셋) {
-  Fs.write(mainPath + room, "{}");
-  replier.reply(room + "방에 저장된 채팅순위가 초기화되었습니다.");
-  replier.reply(time + "부터 기록이 재시작됩니다.");
+    
+    if (msg == ".채팅순위") {
+      
+      arr = file["today"].sort((a,b) => b.times-a.times).map((i) => i.name);
+      //replier.reply(arr)
+      
+      var temp = [];
+      for (i=0; i<arr.length; i++) {
+        temp.push((i+1) + "등: " + arr[i]
+        + "\n채팅: " + file["today"].find(e => e.name == arr[i]).times + "회");
+      }
+      
+     replier.reply(temp.join("\n\n"));
+    }
+    
+    
+    
+  } catch(e) {
+    replier.reply(e + e.lineNumber);
+  }
 }
 
+
+function reset(room) {
+      file = {};
+      file["today"] = [];
+      Fs.write(Path + room, JSON.stringify(file));
+      return;
+}
